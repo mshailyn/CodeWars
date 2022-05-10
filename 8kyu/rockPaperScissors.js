3deac0ac3f6348c16dd9792004dd2f4e62566b23
Shailyn Moore <m.shailyn@yahoo.com>
2022-05-10 14:46:23 -0700

    new codewar

--- 8kyu/rockPaperScissors.js
+++ 8kyu/rockPaperScissors.js
@@ -2,7 +2,6 @@
 // Let's play! You have to return which player won! In case of a draw return Draw!.
 
 // Examples:
-
 // rps('scissors','paper') // Player 1 won!
 // rps('scissors','rock') // Player 2 won!
 // rps('paper','paper') // Draw!
