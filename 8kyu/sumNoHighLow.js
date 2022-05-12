6d6906628472578c354a7cabec71ccd08a259bb4
Shailyn Moore <m.shailyn@yahoo.com>
2022-05-12 14:46:38 -0700

    new codewar

--- 8kyu/sumNoHighLow.js
+++ 8kyu/sumNoHighLow.js
@@ -1,6 +1,7 @@
 // Task
 // Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
 
+
 // The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
 
 // Input validation
