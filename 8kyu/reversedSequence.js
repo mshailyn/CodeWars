322613f531e4014d112cda5d235bd7ad39982e6b
Shailyn Moore <m.shailyn@yahoo.com>
2022-05-15 20:22:03 -0700

    new codewar

---
+++ 8kyu/reversedSequence.js
@@ -0,0 +1,11 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+
+
+const reverseSeq = n => {
+  let newArr = [];
+  for(let i = n; i > 0; i--){
+    newArr.push(i);
+  }
+  return newArr;
+};
