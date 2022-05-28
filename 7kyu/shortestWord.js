b1d9e64bf010f1292e21d412e2a1d8e77e0cdb7a
Shailyn Moore <m.shailyn@yahoo.com>
2022-05-28 09:53:10 -0700

    new codewar

---
+++ 7kyu/shortestWord.js
@@ -0,0 +1,11 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+
+function findShort(s){
+  let short = s.length
+  s.split(' ').map((a) => a.length < short ? short = a.length : short)
+  return short
+}
