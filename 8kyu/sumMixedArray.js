eb0ca3c8943779572f43bac007c3591b43e5c074
Shailyn Moore <m.shailyn@yahoo.com>
2022-05-31 20:29:29 -0700

    new codewar

---
+++ 8kyu/sumMixedArray.js
@@ -0,0 +1,8 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+
+function sumMix(x){
+  return x.reduce((a,c) => ~~a + ~~c , 0)
+}
