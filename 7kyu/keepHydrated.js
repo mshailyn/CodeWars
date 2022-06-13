fa836243b58472069d658320065b09099f84463b
Shailyn Moore <m.shailyn@yahoo.com>
2022-06-13 14:11:11 +0200

    new codewar

---
+++ 7kyu/keepHydrated.js
@@ -0,0 +1,12 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+
+//KEEP HYDRATED - 8KYU
+
+function litres(time) {
+  return Math.floor(0.5 * time);
+}
