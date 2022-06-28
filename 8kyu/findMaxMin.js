7e547a587052211ce9d81435eb42384e39a32709
Shailyn Moore <m.shailyn@yahoo.com>
2022-06-27 21:35:20 -0700

    new codewar

---
+++ 8kyu/findMaxMin.js
@@ -0,0 +1,15 @@
+//Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+
+
+
+
+var min = function(list){
+   list.sort((a,b) => a-b)
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a,b) => b-a)
+    return list[0];
+}
