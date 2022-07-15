d138c999314b8e51db4635dd80fe960057520452
Shailyn Moore <m.shailyn@yahoo.com>
2022-07-15 12:24:09 -0700

    new codewar

--- 8kyu/setAlarm.js
+++ 8kyu/setAlarm.js
@@ -3,8 +3,6 @@
 //The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
 
 
-//8kyu - setAlarm
-
 
 function setAlarm(employed, vacation){
   return employed && !vacation
