d3cfe0931c2b60eb7172d379c958d9844289430b
Shailyn Moore <m.shailyn@yahoo.com>
2022-07-28 13:30:42 -0700

    new codewar

---
+++ 8kyu/calcAverage.js
@@ -0,0 +1,11 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+
+
+
+
+function find_average(array) {
+  return array.length === 0 ? 0 : array.reduce((a,c) => a+c,0)/array.length;
+}
