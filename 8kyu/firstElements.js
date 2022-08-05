924bf61bfe591b7e70a99c22432a104c7c270b5a
Shailyn Moore <m.shailyn@yahoo.com>
2022-08-04 17:25:35 -0700

    new codewar

---
+++ 8kyu/firstElements.js
@@ -0,0 +1,7 @@
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+
+
+function take(arr, n) {
+  return arr.filter((a,i) => i < n)
+}
