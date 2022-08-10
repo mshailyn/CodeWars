f58e2be067f0f7aa9ee889dbbf90ac70a3f62f13
Shailyn Moore <m.shailyn@yahoo.com>
2022-08-09 21:23:22 -0700

    new codewar

---
+++ 8kyu/stringCleaning.js
@@ -0,0 +1,7 @@
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+
+
+function stringClean(s){
+  return s.replace(/[0-9]/g, '')
+}
