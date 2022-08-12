0f82ec514af806711f809ba49a5c2f6f6d35be87
Shailyn Moore <m.shailyn@yahoo.com>
2022-08-11 21:24:46 -0700

    new codewar

---
+++ 8kyu/appleTurnover.js
@@ -0,0 +1,8 @@
+//Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+
+
+
+function apple(x){
+    return Math.pow(x,2) > 1000 ? 'It\'s hotter than the sun!!' : "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
