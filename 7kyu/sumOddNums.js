9aeacdaf8e92e7477f07ce82949a5dcc692b103e
Shailyn Moore <m.shailyn@yahoo.com>
2022-09-07 14:00:58 -0700

    new codewar

---
+++ 7kyu/sumOddNums.js
@@ -0,0 +1,15 @@
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3)
+}
