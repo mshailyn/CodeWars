090de59b77a2fda8e37cd11c1f0b2120b5944f96
Shailyn Moore <m.shailyn@yahoo.com>
2022-10-01 16:52:06 -0700

    new codewar

---
+++ 7kyu/tidyNumber.js
@@ -0,0 +1,9 @@
+// A Tidy number is a number whose digits are in non-decreasing order. Given a number, Find if it is Tidy or not .
+
+
+
+function tidyNumber(n){
+  let ans = true
+ n.toString().split('').forEach((a,i,arr) => a > arr[i+1] ? ans=false : a)
+  return ans
+}
