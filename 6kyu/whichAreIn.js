5a0631ffc5eed7d747bc1a99fbb1c06b4f869338
Shailyn Moore <m.shailyn@yahoo.com>
2022-10-13 12:40:59 -0700

    new codewar

---
+++ 6kyu/whichAreIn.js
@@ -0,0 +1,23 @@
+//Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// Example 2:
+// a1 = ["tarp", "mice", "bull"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns []
+
+
+
+function inArray(array1,array2){
+  let ans = []
+  array1.forEach(a => array2.join(' ').includes(a) ? ans.push(a) : a)
+  return ans.sort()
+}
