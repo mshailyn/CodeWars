da2b10ca628fbfb6268a8f5b1e3f2c9acc77f02a
Shailyn Moore <m.shailyn@yahoo.com>
2022-10-25 12:09:55 -0700

    new codewar

---
+++ 6kyu/arrCombinations.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+
+function solve(arr) {
+    return arr.map(a =>{ return [...new Set([...a])]}).reduce((a,c) => a * c.length, 1)
+  }
