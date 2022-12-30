7683cd11b22701e6ca0b194804df55df1558cbe6
Shailyn Moore <m.shailyn@yahoo.com>
2022-12-30 13:57:21 -0800

    new codewar

---
+++ 7kyu/arrayFilter.js
@@ -0,0 +1,7 @@
+//JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(a => a % 2 === 0)
+}
