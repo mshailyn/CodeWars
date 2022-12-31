f769d9f4fea13c914f8a6f0b2493bbab701166d0
Shailyn Moore <m.shailyn@yahoo.com>
2022-12-31 13:52:34 -0800

    new codewar

---
+++ 8kyu/mergeSortArr.js
@@ -0,0 +1,11 @@
+// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+
+function mergeArrays(arr1, arr2) {
+    let arr = [...new Set([...arr1,...arr2])]
+    return arr.sort((a,b) => a-b)
+   }
