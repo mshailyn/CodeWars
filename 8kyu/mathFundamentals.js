1bcc5ef133356c903f45f7b5496c00e59aaafd3e
Shailyn Moore <m.shailyn@yahoo.com>
2023-01-02 12:04:32 -0800

    new codewar

---
+++ 8kyu/mathFundamentals.js
@@ -0,0 +1,40 @@
+//Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+
+function add(a,b){
+    return a+b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function mod(a,b){
+    return a%b
+}
+   
+function exponent(a,b){
+    return Math.pow(a,b)
+}
+    
+function subt(a,b){
+    return a-b
+}
